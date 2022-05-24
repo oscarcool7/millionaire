8bb380acfbc34684cdd795f4f2f6ee65acea49b2
oscarcool <oscarcool@rambler.ru>
2022-05-24 12:22:08 +0300

    factory_girl replaced by factory_bot

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -5,7 +5,7 @@
 # См. другие примеры на
 #
 # http://www.rubydoc.info/gems/factory_girl/file/GETTING_STARTED.md
-FactoryGirl.define do
+FactoryBot.define do
   factory :question do
     # Последовательность уникальных текстов вопроса
     sequence(:text) { |n| "В каком году была космичесая одиссея #{n}?" }
@@ -14,10 +14,10 @@ FactoryGirl.define do
     sequence(:level) { |n| n % 15}
 
     # Ответы сделаем рандомными для красоты
-    answer1 {"#{rand(2001)}"}
-    answer2 {"#{rand(2001)}"}
-    answer3 {"#{rand(2001)}"}
-    answer4 {"#{rand(2001)}"}
+    answer1 { "#{rand(2001)}" }
+    answer2 { "#{rand(2001)}" }
+    answer3 { "#{rand(2001)}" }
+    answer4 { "#{rand(2001)}" }
   end
 end
 
