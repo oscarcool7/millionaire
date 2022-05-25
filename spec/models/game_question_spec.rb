1f303d7d6f3fcbc1ec6b06f1c68eee564066ec65
oscarcool <oscarcool@rambler.ru>
2022-05-25 17:29:53 +0300

    added test GameQuestion#friend_call

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -96,5 +96,16 @@ RSpec.describe GameQuestion, type: :model do
       expect(ff).to include('b') # должен остаться правильный вариант
       expect(ff.size).to eq 2 # всего должно остаться 2 варианта
     end
+
+    it 'correct friend_call' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+
+      game_question.add_friend_call
+
+      expect(game_question.help_hash).to include(:friend_call)
+      fc = game_question.help_hash[:friend_call]
+
+      expect(fc).to be_a(String)
+    end
   end
 end
